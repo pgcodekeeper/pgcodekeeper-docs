18af7130fb174a1e1e9f78d56aa072631a8b88eb
Elmir Shamsutdinov <shamsutdinov_er@taximaxim.ru>
2023-05-12 11:18:19 +0300

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pgCodeKeeper'
-copyright = '2022, pgCodeKeeper'
+copyright = '2023, pgCodeKeeper'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
